06942faba630ea51bd7bbee8995b247cd70c6119
hemanth mg <134934416+hemanthmg20@users.noreply.github.com>
2025-08-25 13:52:09 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -27,7 +27,7 @@ def create_access_token(data : dict , expire_time : Optional[timedelta] = None):
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_access_token(token : str):
-    return jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
     
 
